10dff7f238192c49d0924a243acee3f5e3434347
Andrea <manzoniandrea08@gmail.com>
2026-03-14 12:34:56 +0100

    esercizio2

---
+++ es2.java
@@ -0,0 +1,10 @@
+public class es2 {
+    public static void main(String[] args) {
+        int Int = 42;
+        double Double = 3.14;
+        String String = "Hello, Java!";
+        System.out.println(Int);
+        System.out.println(Double);
+        System.out.println(String);
+    }
+}
