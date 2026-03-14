49c76d5a2ceb6f22bc3a9c43817eed46c9aa475c
Andrea <manzoniandrea08@gmail.com>
2026-03-14 12:40:20 +0100

    esercizio4

---
+++ es4.java
@@ -0,0 +1,12 @@
+public class es4 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int risultato = somma(a, b);
+        System.out.println("La somma di " + a + " e " + b + " è: " + risultato);
+    }
+
+    public static int somma(int a, int b) {
+        return a + b;
+    }
+}
